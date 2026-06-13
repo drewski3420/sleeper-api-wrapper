c426772a4b506243ca938c7c9362f9d8c3f633ff
Drew <21123428+drewski3420@users.noreply.github.com>
2026-06-13 09:10:17 -0400

    add all players class

---
+++ sleeper_wrapper/all_players.py
@@ -0,0 +1,81 @@
+import os
+import logging
+import json
+
+from .base_api import BaseApi
+from .player import Player
+
+logger = logging.getLogger(__name__)
+
+class AllPlayers(BaseApi):
+  """Retrieves player data from Sleeper."""
+  _cache = None
+
+  def __init__(self, season: int, sport: str = "nfl"):
+    self._sport = sport
+    self._season = season
+    self._filenames = { "projections": {"fn":"projections.json", "endpoint": f"https://api.sleeper.app/v1/players/{self._sport}"}
+                        , "players": {"fn":"players.json", "endpoint": f"https://api.sleeper.com/projections/{self._sport}/{self._season}?season_type=regular&order_by=adp_dynasty_ppr"}
+                      }
+    self.players = self._get_contents(type="players", skip_check=False )
+#    self.projections = self._get_contents(type="projections", skip_check=True)
+
+    if AllPlayers._cache is None:
+      AllPlayers._cache = self._get_contents("players", skip_check=False)
+
+    self.players = AllPlayers._cache
+
+
+  def _get_contents(self, type: str, skip_check: bool) -> Dict:
+    fn = self._filenames[type]['fn']
+    if not os.path.exists(fn) or skip_check:
+      self._populate_file(type, fn)
+    with open(fn,'r') as f:
+      return json.loads(f.read())
+
+  def _populate_file(self, type: str, fn: str) -> dict:
+    url = self._filenames[type]['endpoint']
+    with open(fn, 'w') as f:
+      data = self._call(url)
+      f.write(json.dumps(data))
+
+#  def get_player(self, player_id: str | int) -> Player:
+#    player_id = str(player_id)
+#
+#    metadata = self.players.get(player_id)
+#
+#    if metadata is None:
+#      logger.warning(f"Player {player_id} not found")
+#      return Player(player_id)
+#
+#    return Player(player_id, metadata)
+
+  def get_top_available(self, already_drafted_ids: List[int], sort_by: str, position: List[str] = ["All"]) -> List[dict]:
+    LIMIT = 40
+    i = 0
+    l = []
+    sort_field = f"adp_{sort_by}"
+    for player in self.players:
+      player['stats']['ranked_val'] = (
+        player["stats"].get(sort_field)
+        or player["stats"].get("adp_std")
+        or float("inf")
+      )
+    self.players.sort(key=lambda p: p["stats"]["ranked_val"])
+    for player in self.players:
+      if not player.get('player_id') in already_drafted_ids:
+        player = Player(
+          player_id=player.get('player_id'),
+          player_data={ **player.get("player"), "stats": player.get("stats")}        #player.get('player')
+        )
+        #player = Player(player_id=player.get('player_id'), all_players_data=self.players)
+        if player.position in position or position[0] == 'All':
+          l.append(player)
+          i += 1
+        if i >= LIMIT:
+          break
+#    print(f"Top Available {','.join(position)}")
+#    for p in l:
+#      print(f"Name: {p['first_name']} {p['last_name']} - Position: {p['position']} -  Team: {p['team']} - Exp: {p['years_exp']} - ADP: {p['adp_dynasty_std']}")
+#
+    return l
