d285d951f6be8f35ffaf09929b4103dce8c50d1a
Drew <21123428+drewski3420@users.noreply.github.com>
2026-06-14 16:07:43 -0400

    feat: model transaction teams, players, and draft picks

--- sleeper_wrapper/transaction.py
+++ sleeper_wrapper/transaction.py
@@ -21,6 +21,38 @@ from .player import Player
 'status_updated': 1757577658636, 
 'waiver_budget': []
 """
+class TransactionPlayer:
+  def __init__(self, player_id: int):
+    self.player_id = player_id
+
+class TransactionPick:
+  def __init__(self, data: dict):
+    self._data = data
+    self.round_number = self._data['round']
+    self.season = int(self._data['season'])
+    self.old_roster_id = int(self._data['previous_owner_id'])
+    self.new_roster_id = int(self._data['owner_id'])
+
+class TransactionTeam:
+  def __init__(
+      self,
+      team_num: str,
+      roster_id: int
+  ):
+    self.team_num = team_num
+    self.roster_id = roster_id
+
+    # populate later
+    self.team = None
+    self.user = None
+    self.players_added = []
+    self.players_dropped = []
+    self.picks_added = []
+    self.picks_lost = []
+
+#  def __str__(self):
+#    return f"{self.team_num}: {self.roster_id}"
+
 class Transaction:
   def __init__(self, data: dict):
     self._data = data
@@ -32,18 +64,43 @@ class Transaction:
     self.creator = data.get("creator")
     self.created = datetime.fromtimestamp(data.get("created")/1000)
 
-    self.roster_ids = data.get("roster_ids", [])
+    self.teams = [
+      TransactionTeam(
+        team_num=f"team{i}",
+        roster_id=roster_id
+      )
+      for i, roster_id in enumerate(
+          data.get("roster_ids", []),
+          start=1
+      )
+    ]
+    self.teams_by_roster_id = {team.roster_id: team for team in self.teams}
 
-    self.settings = data.get("settings")
     self.adds = data.get("adds") or {}
     self.drops = data.get("drops") or {}
+    self._populate_adds()
+    self._populate_drops()
+    self._populate_picks()
 
-  def __repr__(self):
-    return (
-      f"{self.__class__.__name__}"
-      f"(id={self.transaction_id}, status={self.status},transaction_type={self.transaction_type})"
-    )
+  def _populate_picks(self) -> None:
+    for p in self._data.get('draft_picks',[]):
+      pick = TransactionPick(p)
+      self.teams_by_roster_id[pick.new_roster_id].picks_added.append(pick)
+      self.teams_by_roster_id[pick.old_roster_id].picks_lost.append(pick)
 
+  def _populate_drops(self) -> None:
+    for player_id, roster_id in self.drops.items():
+      self.teams_by_roster_id[roster_id].players_dropped.append(TransactionPlayer(player_id))
+
+  def _populate_adds(self) -> None:
+    for player_id, roster_id in self.adds.items():
+      self.teams_by_roster_id[roster_id].players_added.append(TransactionPlayer(player_id))
+
+#  def __repr__(self):
+#    return (
+#      f"{self.__class__.__name__}"
+#      f"(id={self.transaction_id}, status={self.status},transaction_type={self.transaction_type})"
+#    )
 
 class Trade(Transaction):
   def __init__(self, data: dict):
@@ -55,28 +112,26 @@ class Trade(Transaction):
     self.adds = data.get("adds") or {}
     self.drops = data.get("drops") or {}
 
-  def __str__(self):
-    return (
-      f"Trade("
-      f"rosters={self.roster_ids}, "
-      f"picks={len(self.draft_picks)}"
-      f")"
-    )
+#  def __str__(self):
+#    return (
+#      f"Trade("
+#      f"rosters={self.roster_ids}, "
+#      f"picks={len(self.draft_picks)}"
+#      f")"
+#    )
 
 class FreeAgent(Transaction):
   def __init__(self, data: dict):
     super().__init__(data)
 
-#    self.adds = data.get("adds") or {}
-    self.drops = data.get("drops") or {}
-    self.message = "Free Agent"
-  def __str__(self):
-    return (
-      f"FreeAgent("
-      f"adds={len(self.adds)}, "
-      f"drops={len(self.drops)}"
-      f")"
-    )
+#  #Free Agent has no additional properties
+#  def __str__(self):
+#    return (
+#      f"FreeAgent("
+#      f"adds={len(self.adds)}, "
+#      f"drops={len(self.drops)}"
+#      f")"
+#    )
 
 
 class Waiver(Transaction):
@@ -91,11 +146,13 @@ class Waiver(Transaction):
     self.waiver_bid = (int(data.get("settings", {}).get("waiver_bid")) if data.get("settings") else None)
     self.waiver_seq = (int(data.get("settings", {}).get("seq")) if data.get("settings") else None)
 
-  def __str__(self):
-    return (
-      f"Waiver("
-      f"bid={self.waiver_bid}, "
-      f"adds={len(self.added_player_ids)}, "
-      f"drops={len(self.dropped_player_ids)}"
-      f")"
-    )
+    self.message = data.get('metadata',{}).get('notes')
+
+#  def __str__(self):
+#    return (
+#      f"Waiver("
+#      f"bid={self.waiver_bid}, "
+#      f"adds={len(self.added_player_ids)}, "
+#      f"drops={len(self.dropped_player_ids)}"
+#      f")"
+#    )
